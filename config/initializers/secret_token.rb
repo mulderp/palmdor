b1ebd59296ddfd1c700bfe9a78d0af17a6de40d5
Patrick Mulder <mulder.patrick@gmail.com>
2013-06-03 23:02:26 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palmdor::Application.config.secret_key_base = 'c0fb763c2bf4dd868cbc1266008226b76310ac72ab1c77418bac2b39758c13af3212ee79194b127bfd5b9e2df3c68b1394fe8cba5d86ab83162cb1663815f4aa'
