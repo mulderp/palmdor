0c00d04382d91e146ff388b229d86b8b3d31108c
Patrick Mulder <mulder.patrick@gmail.com>
2013-06-07 23:39:27 +0200

    playing with views

--- app/assets/javascripts/modules/app.module.js
+++ app/assets/javascripts/modules/app.module.js
@@ -8,13 +8,55 @@ var MoviesView = Backbone.View.extend({
 
 });
 
+var DetailsView = Backbone.View.extend({
+  el: $('#details'),
+  template: 'templates/details',
+  render: function() {
+    this.$el.html(this.movie);
+  },
+  initialize: function(opts) {
+    this.movie = opts.movie;
+    this.render();
+  }
+});
+
+var MovieView = Backbone.View.extend({
+
+  events: {
+    "click": "handleClick"
+  },
+
+  handleClick: function(ev) {
+    var movie = $(ev.currentTarget).data('movie');
+    this.trigger("show:movie", movie);
+  },
+
+  initialize: function(opts) {
+    console.log("show");
+  }
+
+});
+
 var Controller = function() {
 
   this.showMovie = function(id) {
-    console.log('showMovie');
-    var movie = new MoviesView();
-  }
+    var movie = new DetailsView({movie: id});
+  };
 
+  this.start = function() {
+    var movies = $("#movies");
+    var that = this;
+    movies.masonry();
+
+    _.each(movies.find('article'), function(movie) {
+      var movieItem = new MovieView({el: movie});
+      movieItem.on('show:movie', function(movie) {
+        that.showMovie(movie);
+      });
+    });
+
+  };
 }
+
 
 module.exports = Controller;
