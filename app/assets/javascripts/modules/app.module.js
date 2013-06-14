48571f84375efe011b5654451fb52c150a1dbfaf
Patrick Mulder <mulder.patrick@gmail.com>
2013-06-14 12:34:47 +0200

    Clean up views and logic

--- app/assets/javascripts/modules/app.module.js
+++ app/assets/javascripts/modules/app.module.js
@@ -1,13 +1,5 @@
 var FilterView = require('./views/filter_view');
 
-var MoviesView = Backbone.View.extend({
-  events: {},
-
-  initialize: function() {
-    console.log("show");
-  }
-});
-
 var DetailsView = Backbone.View.extend({
 
   el: $('#details'),
@@ -15,12 +7,11 @@ var DetailsView = Backbone.View.extend({
   template: 'details',
 
   render: function() {
-    var tmpl = HandlebarsTemplates[this.template]({title: this.title});
+    var tmpl = HandlebarsTemplates[this.template](this.model);
     this.$el.html(tmpl);
   },
 
   initialize: function(opts) {
-    this.title = opts.movie;
     this.controller = opts.controller;
     this.render();
   }
@@ -33,6 +24,8 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function(ev) {
+    ev.preventDefault();
+    console.log("show");
     var movie = $(ev.currentTarget).data('movie');
     this.trigger("show:movie", movie);
   },
@@ -46,43 +39,56 @@ var MovieView = Backbone.View.extend({
 var Controller = function() {
 
   this.showMovie = function(movie) {
-    var movie = new DetailsView({movie: movie, controller: this});
+    new DetailsView({model: movie, controller: this});
   };
 
   this.showPrevious = function() {
-    this.selected -= 1;
-    this.showMovie(this.movie_ids[this.selected]);
+    if (this.selected > 0) {
+      this.selected -= 1;
+    }
+    else
+    {
+      this.selected = _.size(this.movies) - 1;
+    }
+    this.showMovie(this.movies[this.selected]);
   };
 
   this.showNext = function(id) {
-    this.selected += 1;
+    if (this.selected < (_.size(this.movies) - 1)) {
+      this.selected += 1;
+    }
+    else
+    {
+      this.selected = 0;
+    }
+
     console.log(this.selected);
-    this.showMovie(this.movie_ids[this.selected]);
+    this.showMovie(this.movies[this.selected]);
   };
 
   this.start = function() {
     _.bindAll(this, 'showNext', 'showPrevious');
 
-    var movies = $("#movies").find('article');
+    var $movies = $("#movies").find('article');
 
-    var idx = 0;
-    this.movie_ids = _.map(movies, function(movie) {
-      idx += 1;
-      return $(movie).data('movie');
+    this.movies = _.map($movies, function(movie) {
+      var title = $(movie).find(".title").text();
+      var description = $(movie).find(".description").text();
+      return { title: title, slug: $(movie).data('movie'), description: description };
     });
 
     var that = this;
-    $('#movies').masonry();
-
-    _.each(movies, function(movie) {
+    _.each($movies, function(movie) {
       var movieItem = new MovieView({el: movie});
-      movieItem.on('show:movie', function(movie) {
+      movieItem.on('show:movie', function(slug) {
+        var movie = _.where(that.movies, {slug: slug})[0];
+        that.selected = _.indexOf(that.movies, movie);
         that.showMovie(movie);
       });
     });
 
     this.selected = 0;
-    this.showMovie(this.movie_ids[0]);
+    this.showMovie(this.movies[0]);
     var filterView = new FilterView({el: $("#filter"), controller: this});
   };
 }
